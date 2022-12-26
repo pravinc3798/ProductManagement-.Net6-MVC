a1db2133ddb01a6663ced0c546b05a0d19c49fd2
pravinc3798 <pravin.choudhary88@gmail.com>
2022-12-26 12:37:44 +0530

    added some css to views. removed default home controller.

--- CommonLayer/Model/ProductModel.cs
+++ CommonLayer/Model/ProductModel.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Serialization.Formatters;
-using Microsoft.AspNetCore.Http.Internal;
-using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommonLayer.Model
 {
     public class ProductModel
     {
         public int ProductId { get; set; }
+        [DisplayName("Code")]
+        [StringLength(25)]
         public string ProductCode { get; set; } = null!;
+        [DisplayName("Name")]
+        [StringLength(250)]
         public string ProductName { get; set; } = null!;
+        [DisplayName("Description")]
+        [StringLength(4000)]
         public string ProductDescription { get; set; } = null!;
+        [DisplayName("Expiry Date")]
+        [DataType(DataType.Date)]
         public DateTime ExpiryDate { get; set; }
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
+        [DisplayName("Image")]
         public IFormFile ProductImage { get; set; }
+        [DisplayName("Status")]
         public bool IsActive { get; set; }
         public DateTime CreationDate { get; set; }
     }
--- RepositoryLayer/Entity/TblProduct.cs
+++ RepositoryLayer/Entity/TblProduct.cs
@@ -6,23 +6,12 @@ namespace RepositoryLayer.Entity;
 public partial class TblProduct
 {
     public int ProductId { get; set; }
-    [DisplayName("Product Code")]
-    [StringLength(25)]
     public string ProductCode { get; set; } = null!;
-    [DisplayName("Name")]
-    [StringLength(250)]
     public string ProductName { get; set; } = null!;
-    [DisplayName("Description")]
-    [StringLength(4000)]
     public string ProductDescription { get; set; } = null!;
-    [DisplayName("Expiry Date")]
-    [DataType(DataType.Date)]
     public DateTime ExpiryDate { get; set; }
-    [DisplayName("Category")]
     public int CategoryId { get; set; }
-    [DisplayName("Image")]
     public string? ProductImage { get; set; }
-    [DisplayName("Status")]
     public bool? IsActive { get; set; }
 
     public DateTime CreationDate { get; set; }
